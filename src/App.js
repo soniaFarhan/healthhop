c88cff71ef2277a758b4c76c05b53d99321d71d1
usman-fabtechsol <usman@fabtechsol.com>
2023-09-15 11:14:46 +0500

    iamges added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
 import { Layout } from './components/Layout';
 import { Home } from './pages/Home';
+import { ContactUs } from './pages/ContactUs';
 
 
 
@@ -9,11 +10,9 @@ function App() {
   return (<div>
     <Routes>
       <Route element={<Layout/>}>
-
-        
         <Route path='/' element={<Home/>}/>
-        
-        </Route>
+        <Route path='/contact-us' element={<ContactUs/>} />        
+      </Route>
     </Routes>
 
 

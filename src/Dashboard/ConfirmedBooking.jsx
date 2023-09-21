4fbe4a4e73d0c2f2080e167de7dcbd1e32bc7332
Husnainp <husnain483271@gmail.com>
2023-09-21 17:53:07 +0500

    pages

--- src/Dashboard/ConfirmedBooking.jsx
+++ src/Dashboard/ConfirmedBooking.jsx
@@ -1,7 +1,131 @@
+import { Chat } from '@mui/icons-material'
+import { Button } from '@mui/material'
 import React from 'react'
 
 export const ConfirmedBooking = () => {
+
+const data =[
+{
+name:"Herry",
+chat:"         Hair transplant surgery is a procedure used to treat hair loss. Various techniques are available, but all hair transplants involve taking hair-bearing No comments on this screen skin from one part of the scalp and grafting these pieces of skin onto bald or thinning areas of the scalp or areas of trauma....... Read more",
+img:
+<img src={require("../assets/blog-card-4.png")} width={"100%"} alt="" />,
+amount:" $ 1000",
+orderno:"002",
+date:" 002"
+},
+{
+name:"Herry",
+chat:"         Hair transplant surgery is a procedure used to treat hair loss. Various techniques are available, but all hair transplants involve taking hair-bearing No comments on this screen skin from one part of the scalp and grafting these pieces of skin onto bald or thinning areas of the scalp or areas of trauma....... Read more",
+img:
+<img src={require("../assets/blog-card-4.png")} width={"100%"} alt="" />,
+amount:" $ 1000",
+orderno:"002",
+date:" 002"
+
+},
+{
+name:"Herry",
+chat:"         Hair transplant surgery is a procedure used to treat hair loss. Various techniques are available, but all hair transplants involve taking hair-bearing No comments on this screen skin from one part of the scalp and grafting these pieces of skin onto bald or thinning areas of the scalp or areas of trauma....... Read more",
+img:
+<img src={require("../assets/blog-card-4.png")} width={"100%"} alt="" />,
+amount:" $ 1000",
+orderno:"002",
+date:" 002"
+
+},
+
+
+
+
+]
+
+
   return (
-    <div>ConfirmedBooking</div>
+    <>
+    <div className="container my-5  pt-2 shadow" style={{borderRadius:"12px",paddingBottom:"90px"}}>
+
+{
+  data.map((e)=>{
+    return(
+
+      <div className="">
+
+
+<div className=" row shadow m-2 p-1 " style={{borderRadius:"12px"}}>
+
+
+
+
+
+
+<div className="col-md-2">
+  <div className="" style={{width:"8rem"}}>
+
+{e.img}
+  </div>
+
+
+</div>
+<div className="col-md-10 ">
+
+<div className="d-flex justify-content-between pt-1 aling-items-center">
+  <h6 style={{fontSize:"13px"}}>  {e.name} </h6>
+  
+  
+   <p style={{fontSize:"8px",color:"#07A6A9"}}></p>
+</div>
+
+<div className="">
+<p style={{fontSize:"12px"}}>
+{e.chat}
+</p>
+
+</div>
+<div className="d-flex aling-items-center justify-content-between">
+
+<div className="d-flex justify-content-between">
+
+<p className='my-auto' style={{fontSize:"12px"}}>  <span className='fw-bold'>  Amount:  </span> { e.amount } </p>
+<p className='my-auto mx-4' style={{fontSize:"12px"}}>  <span className='fw-bold'>Order No</span> { e.orderno}</p>
+<p className='my-auto ' style={{fontSize:"12px"}}> <span className='fw-bold'>Scheduled pro:</span>  {   e.date }</p>
+
+
+
+ </div>
+
+
+<div className="d-flex aling-items-center">
+<div className="m-2">
+
+<Button variant="text" style={{color:'#07A6A9'}} ><Chat/></Button>
+  </div>
+
+<div className="m-2">
+
+<Button variant="contained" style={{backgroundColor:"#07A6A9",padding:"6px 27px",fontSize:"10px"}}>Meet</Button>
+</div>
+</div>
+ 
+
+</div>
+
+</div>
+
+
+</div>
+      </div>
+
+    )
+  })
+}
+
+
+
+    </div>
+    
+    
+    </>
   )
 }
+
--- src/Dashboard/Packges.jsx
+++ src/Dashboard/Packges.jsx
@@ -2,6 +2,98 @@ import React from 'react'
 
 export const Packges = () => {
   return (
-    <div>Packges</div>
+    <>
+
+<div className="container">
+
+  <div className="d-flex justify-content-between aling-items-center my-3 ">
+<div className="">
+<h1>Packages</h1>
+  
+</div>
+<div className="my-auto">
+<div className='my-auto'>
+
+<button className='btn my-auto ' style={{backgroundColor:"#07A6A9",color:"white"}}>add Package</button>
+</div>
+</div>
+
+
+  </div>
+<div className="row justify-content-between">
+<div className="col-md-6 my-1 ">
+<div class="card mx-auto   "style={{ maxWidth: 300, minHeight: 400 }}>
+  <div class="card-body d-flex flex-column justify-content-between">
+    <div className="text-center p-3" style={{backgroundColor:"#07A6A9",color:"white",borderRadius:"12px"}}>
+      <p>HAIR TRANSPLANT</p>
+      <h1>$10,000</h1>
+    </div>
+   <div className="" >
+<p className='font-weight-light' style={{fontSize:"12px"}}>
+Hair transplant procedure Hair transplant procedure
+
+Hotel accommodation for 3 nights Hotel accommodation for 3 nights
+Transfer to and from airport/hotel/ Transfer to and from airport/hotel/
+clinic clinic
+
+After care</p>
+
+   </div>
+   <div className='mb-3' >
+                        <select class="form-select py-3" aria-label="Default select example"  style={{backgroundColor:"#07A6A9",color:"white"}}>
+                        <option selected> send to patient</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                        </select>
+                    </div>
+
+   
+  </div>
+</div>
+</div>
+<div className="col-md-6 my-2">
+<div class="card mx-auto   "style={{ maxWidth: 300, minHeight: 400 }}>
+  <div class="card-body d-flex flex-column justify-content-between">
+    <div className="text-center p-3" style={{backgroundColor:"#07A6A9",color:"white",borderRadius:"12px"}}>
+      <p>HAIR TRANSPLANT</p>
+      <h1>$10,000</h1>
+    </div>
+   <div className="" >
+<p className='font-weight-light' style={{fontSize:"12px"}}>
+Hair transplant procedure Hair transplant procedure
+
+Hotel accommodation for 3 nights Hotel accommodation for 3 nights
+Transfer to and from airport/hotel/ Transfer to and from airport/hotel/
+clinic clinic
+
+After care</p>
+
+   </div>
+   <div className='mb-3' >
+                        <select class="form-select py-3" aria-label="Default select example"  style={{backgroundColor:"#07A6A9",color:"white"}}>
+                        <option selected> send to patient</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                        </select>
+                    </div>
+
+   
+  </div>
+</div>
+</div>
+
+
+
+</div>
+
+</div>
+
+
+
+
+
+    </>
   )
 }

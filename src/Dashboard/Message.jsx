c181b18d1d29096a130a6c834bda3adba2467005
Waleed <sulehri6373@gmail.com>
2023-10-04 15:02:55 +0500

    message incomplete

--- src/Dashboard/Message.jsx
+++ src/Dashboard/Message.jsx
@@ -1,23 +1,58 @@
-import { Avatar } from '@mui/material'
+
 import React from 'react'
 import { PiPaperPlaneRightBold } from 'react-icons/pi'
+import { AiFillMinusCircle } from 'react-icons/ai'
+import { BsDot } from 'react-icons/bs'
 import { SlCalender } from 'react-icons/sl'
 import PopupCalender from '../components/PopupCalender'
-
+import { styled } from '@mui/material/styles';
+import Badge from '@mui/material/Badge';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
 
 export const Message = () => {
-
+  const StyledBadge = styled(Badge)(({ theme }) => ({
+    '& .MuiBadge-badge': {
+      backgroundColor: '#44b700',
+      color: '#44b700',
+      boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+      '&::after': {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        borderRadius: '50%',
+        border: '1px solid currentColor',
+        content: '""',
+      },
+    },
+    
+  }));
   const data = [
     {
-      avater: <Avatar sx={{ width: 55, height: 55 }} />,
+      // avater: <img className='rounded-circle' style={{objectFit:"cover", width: 55, height: 55}} src={require("../assets/card-2.jpg")} alt=""  />,
+      avater:  <StyledBadge
+      overlap="circular"
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      variant="dot"
+    >
+      <Avatar style={{width: 55, height: 55}} alt="Remy Sharp" src={require("../assets/card-2.jpg")} />
+    </StyledBadge>,
       name: "Farnky Schmid",
       inquire: <span style={{color:"rgb(249,9,9)"}}>New Inquiry</span>,
       chat: "Me: what does This Dimmy",
-      date: "30.2.2021"
+      date: <AiFillMinusCircle style={{fontSize:"20px", color:"rgb(249,9,9)"}}/>
 
     },
     {
-      avater: <Avatar sx={{ width: 55, height: 55 }} />,
+      avater:  <StyledBadge
+      overlap="circular"
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      variant="dot"
+    >
+      <Avatar style={{width: 55, height: 55}} alt="Remy Sharp" src={require("../assets/card-1.jpg")} />
+    </StyledBadge>,
       name: "SAHSA COHEN",
       inquire: <span style={{color:"rgb(243,174,36)"}}>Responded</span>,
       chat: "Typing...",
@@ -25,7 +60,13 @@ export const Message = () => {
 
     },
     {
-      avater: <Avatar sx={{ width: 55, height: 55 }} />,
+      avater:  <StyledBadge
+      overlap="circular"
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      variant="dot"
+    >
+      <Avatar style={{width: 55, height: 55}} alt="Remy Sharp" src={require("../assets/card-3.jpg")} />
+    </StyledBadge>,
       name: "ROBERT LANDSBERG",
       inquire:<span style={{color:"rgb(0,114,180)"}}>Quote Offered</span>,
       chat: "See you soon!",
@@ -33,7 +74,13 @@ export const Message = () => {
 
     },
     {
-      avater: <Avatar sx={{ width: 55, height: 55 }} />,
+      avater:  <StyledBadge
+      overlap="circular"
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      variant="dot"
+    >
+      <Avatar style={{width: 55, height: 55}} alt="Remy Sharp" src={require("../assets/Ellipse 1.png")} />
+    </StyledBadge>,
       name: "CRISTINA ROHMER",
       inquire: <span style={{color:"rgb(101,129,68)"}}>Accepted and Confirmed</span>,
       chat: "That ws wonderful. Thanks...",
@@ -41,7 +88,13 @@ export const Message = () => {
 
     },
     {
-      avater: <Avatar sx={{ width: 55, height: 55 }} />,
+      avater:  <StyledBadge
+      overlap="circular"
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      variant="dot"
+    >
+      <Avatar style={{width: 55, height: 55}} alt="Remy Sharp" src={require("../assets/md-1.jpg")} />
+    </StyledBadge>,
       name: "LUKAS SCHULTZ",
       // inquire: "New Inquiry",
       chat: "Alles klar!",
@@ -49,7 +102,13 @@ export const Message = () => {
 
     },
     {
-      avater: <Avatar sx={{ width: 55, height: 55 }} />,
+      avater:  <StyledBadge
+      overlap="circular"
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      variant="dot"
+    >
+      <Avatar style={{width: 55, height: 55}} alt="Remy Sharp" src={require("../assets/md-5.jpg")} />
+    </StyledBadge>,
       name: "BOBBY BEUGER",
       // inquire: "New Inquiry",
       chat: "Was machst du?",
@@ -57,7 +116,13 @@ export const Message = () => {
 
     },
     {
-      avater: <Avatar sx={{ width: 55, height: 55 }} />,
+      avater:  <StyledBadge
+      overlap="circular"
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      variant="dot"
+    >
+      <Avatar style={{width: 55, height: 55}} alt="Remy Sharp" src={require("../assets/pleased-young-female-doctor-wearing-medical-robe-stethoscope-around-neck-standing-with-closed-posture.png")} />
+    </StyledBadge>,
       name: "BOBBY BEUGER",
       // inquire: "New Inquiry",
       chat: "Was machst du?",
@@ -90,10 +155,13 @@ export const Message = () => {
 
 
                       <div className="d-flex aling-items-center justify-content-between   shadows m-2" style={{ borderRadius: "10px" }}>
-                        <div className="d-flex py-2">
-                          <div className='mx-2 my-auto'>
+                        <div className="d-flex py-2 ">
+                          <div className='mx-2 my-auto position-relative'>
                             {e.avater}
+                            <span className='position-absolute' style={{top:"10px", left:"10px"}}>{e.dot}</span>
+
                           </div>
+
                           <div className="my-auto">
                             <p className='m-0 fw-bold ' style={{ fontSize: "11px" }}>{e.name}</p>
                             <p className='m-0 ' style={{ fontSize: "11px" }}> {e.chat} </p>
@@ -101,7 +169,7 @@ export const Message = () => {
                         </div>
 
                         <div>
-                          <p className='m-0 fw-light pt-2 text-nowrap' style={{ fontSize: "9px" }}>{e.inquire}</p>
+                          <p className='m-0  pt-2 text-nowrap pe-1 fw-semibold' style={{ fontSize: "9px" }}>{e.inquire}</p>
                           <p className='m-0 fw-light my-auto p-2 float-end' style={{ fontSize: "9px", maxWidth:"max-content" }}>{e.date}</p>
 
                         </div>
@@ -135,25 +203,21 @@ export const Message = () => {
                           <p className='mt-2 p-2 fw-light ms-auto' style={{ backgroundColor: "rgb(251,253,251)", borderRadius: "10px", color: "#999A99", fontSize: "12px", maxWidth:"fit-content" }}>Hello Franky!</p>
                         </div>
 
-
-
-
-
                       </div>
                       <div className="word" >
                         <div className='ms-auto'>
                           <p className='mt-2 p-2 fw-light word' style={{ backgroundColor: "rgb(251,253,251)", borderRadius: "10px", color: "#999A99", fontSize: "12px", maxWidth:"fit-content"  }}>What are you doing today?</p>
                         </div>
-
-
-
-
-
+                      </div>
+                      <div className="word" >
+                        <div className='ms-auto'>
+                          <p className=' fw-light word float-end' style={{ backgroundColor: "white", borderRadius: "10px", color: "green", fontSize: "12px", maxWidth:"fit-content"  }}>09:45</p>
+                        </div>
                       </div>
                     </div>
                     <div className='mx-2 '>
 
-                      <Avatar sx={{ width: 40, height: 40 }} />
+                    <img className='rounded-circle' style={{objectFit:"cover", width: 40, height: 40}} src={require("../assets/md-5.jpg")} alt=""  />
                     </div>
 
 
@@ -161,19 +225,18 @@ export const Message = () => {
                   <div className="d-flex justify-content- ">
                       <div className='mx-2 '>
 
-                        <Avatar sx={{ width: 40, height: 40 }} />
+                      <img className='rounded-circle' style={{objectFit:"cover", width: 40, height: 40}} src={require("../assets/card-2.jpg")} alt=""  />
                       </div>
 
                       <div className="word" >
                         <p className='mt-2 p-2 fw-light' style={{ backgroundColor: "#FFFFF", borderRadius: "10px", color: "#999A99", fontSize: "12px", maxWidth: "fit-content", border: "1px solid rgb(90,156,77)" }}>Hi George! nice to hear you again!</p>
-
-
-
                       </div>
-
-
-
                   </div>
+                  <div className="word" >
+                        <div className='ms-auto'>
+                          <p className=' fw-light word ' style={{ backgroundColor: "white", borderRadius: "10px", color: "green", fontSize: "12px", maxWidth:"fit-content"  }}>09:47</p>
+                        </div>
+                      </div>
                   <div className="d-flex justify-content-end ">
                     <div className="word" style={{ maxWidth: "40%" }} >
 
@@ -188,7 +251,7 @@ export const Message = () => {
                     </div>
                     <div className='mx-2 '>
 
-                      <Avatar sx={{ width: 40, height: 40 }} />
+                    <img className='rounded-circle' style={{objectFit:"cover", width: 40, height: 40}} src={require("../assets/md-5.jpg")} alt=""  />
                     </div>
 
 
